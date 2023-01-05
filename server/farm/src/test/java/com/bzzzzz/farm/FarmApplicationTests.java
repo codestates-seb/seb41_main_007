fd657e5c8c1a40530f70127a48e7399b6c263b70
bbamsooexit <hiaro66@naver.com>
2023-01-05 11:06:57 +0900

    [BE Feat]초기 세팅

---
+++ server/farm/src/test/java/com/bzzzzz/farm/FarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bzzzzz.farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
