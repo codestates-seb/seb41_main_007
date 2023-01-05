7531f810e73b870272ce205e1ff5d96df06ce4e4
sktjgudals <sktjgudals@naver.com>
2023-01-05 17:56:11 +0900

    [FE Feat] 컴포넌트 분리및 스타일링

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Header from 'Components/Header';
-import Main from 'Pages/Main';
-import Loding from 'Components/Loding';
-import login from 'Pages/Login';
+import Header from './Components/Header/index';
+import Main from './Pages/Main';
+import Loding from './Components/Loding';
+import login from './Pages/Login';
 
 const withLayout = (Component: React.FC): JSX.Element => {
   return (
