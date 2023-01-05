80a8e9d589b93d00140c74c55f6d27e62d40ff8b
nakjun12 <wns450@naver.com>
2023-01-05 17:12:37 +0900

    [FE Feat] Login, signup 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Header from './Components/Header/index';
-import Main from './Pages/Main';
-import Loding from './Components/Loding';
-import login from './Pages/Login';
-
+import Header from 'Components/Header';
+import Main from 'Pages/Main';
+import Loding from 'Components/Loding';
+import Login from 'Pages/Login';
+import SingUp from 'Pages/Signup';
 const withLayout = (Component: React.FC): JSX.Element => {
   return (
     <>
@@ -22,8 +22,12 @@ const router = createBrowserRouter([
     errorElement: <Loding />,
   },
   {
-    path: '/:questionId',
-    element: withLayout(login),
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/singup',
+    element: <SingUp />,
   },
 ]);
 
---
+++ client/src/Pages/Signup.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Signup: React.FC = () => {
+  return <div></div>;
+};
+
+export default Signup;
