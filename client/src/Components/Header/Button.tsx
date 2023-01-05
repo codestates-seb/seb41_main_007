89bb1d6852b1a49672dcb8e51949893e3696fdaa
sktjgudals <sktjgudals@naver.com>
2023-01-05 17:56:11 +0900

    [FE Feat] 컴포넌트 분리및 스타일링

--- client/src/Components/Header/Button.tsx
+++ client/src/Components/Header/Button.tsx
@@ -1,20 +1,25 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const Button: FC = () => {
   return (
     <>
-      <a
-        href="#!"
-        className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+      <Link
+        to="/"
+        className="text-gray-800 dark:text-white hover:bg-gray-50
+        focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4
+        py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700
+        focus:outline-none dark:focus:ring-gray-800"
       >
         Login
-      </a>
-      <a
-        href="#!"
+      </Link>
+
+      <Link
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        to="/"
       >
         Sign up
-      </a>
+      </Link>
     </>
   );
 };
