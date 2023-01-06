d80737936526c20c5c84c9b2fd379324fe2b51f7
FordangIT <fordang0819@gmail.com>
2023-01-06 14:53:20 +0900

    [FE Feat]: signup 초기화

--- client/src/Pages/Signup.tsx
+++ client/src/Pages/Signup.tsx
@@ -1,42 +1,44 @@
-import useInput from 'CustomHook/useInput';
-import React from 'react';
-import styled from 'styled-components';
-export const Form = styled.form``;
-export const Id = styled.div``;
-export const PassWord = styled.div``;
-export const Name = styled.div``;
-export const Birth = styled.div``;
-export const Gender = styled.div``;
-export const Email = styled.div``;
-export const Phone = styled.div``;
+// import useInput from 'CustomHook/useInput';
+// import React from 'react';
+// import styled from 'styled-components';
+// export const Form = styled.form``;
+// export const Id = styled.div``;
+// export const PassWord = styled.div``;
+// export const Name = styled.div``;
+// export const Birth = styled.div``;
+// export const Gender = styled.div``;
+// export const Email = styled.div``;
+// export const Phone = styled.div``;
 const Signup: React.FC = () => {
-  const [idInput, setIdInput] = useInput('');
-  const [passwordInput, setPasswordInput] = useInput('');
-  const [rePasswordInput, setRePasswordInput] = useInput('');
-  const [nameInput, setNameInput] = useInput('');
-  //생년월일
-  //성별
-  const [emailInput, setEmailInput] = useInput('');
-  //휴대전화
-  return (
-    <>
-      <Form>
-        <Id>
-          <label htmlFor="user_id">
-            아이디 <span className="user_id--text"></span>
-          </label>
-          <div>
-            <input type="id" name="user_id" id="user_id"></input>
-          </div>
-        </Id>
-        <PassWord></PassWord>
-        <Name></Name>
-        <Birth></Birth>
-        <Gender></Gender>
-        <Email></Email>
-        <Phone></Phone>
-      </Form>
-    </>
-  );
+  return <div></div>;
+
+  //   const [idInput, setIdInput] = useInput('');
+  //   const [passwordInput, setPasswordInput] = useInput('');
+  //   const [rePasswordInput, setRePasswordInput] = useInput('');
+  //   const [nameInput, setNameInput] = useInput('');
+  //   //생년월일
+  //   //성별
+  //   const [emailInput, setEmailInput] = useInput('');
+  //   //휴대전화
+  //   return (
+  //     <>
+  //       <Form>
+  //         <Id>
+  //           <label htmlFor="user_id">
+  //             아이디 <span className="user_id--text"></span>
+  //           </label>
+  //           <div>
+  //             <input type="id" name="user_id" id="user_id"></input>
+  //           </div>
+  //         </Id>
+  //         <PassWord></PassWord>
+  //         <Name></Name>
+  //         <Birth></Birth>
+  //         <Gender></Gender>
+  //         <Email></Email>
+  //         <Phone></Phone>
+  //       </Form>
+  //     </>
+  //   );
 };
 export default Signup;
