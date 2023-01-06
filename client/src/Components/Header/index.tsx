80685ab0a505e6dae5fa90c290531f05936e08af
sktjgudals <sktjgudals@naver.com>
2023-01-06 18:10:29 +0900

    [FE MISC] Link 연결

--- client/src/Components/Header/index.tsx
+++ client/src/Components/Header/index.tsx
@@ -4,15 +4,18 @@ import LoginRequiredButton from './LoginRequiredButton';
 import LoggedInButton from './LoggedInButton';
 import Input from './Input';
 import './Styles/index.css';
+import { Link } from 'react-router-dom';
 
 const Header: FC = () => {
-  let isUser = false;
+  let isUser = true;
   return (
     <header>
       <div className="Header_Container">
         <div className="Header_Contents_Container">
           <div className="Header_Logo_Container">
-            <Logo width={290} height={290} />
+            <Link to="/">
+              <Logo width={290} height={290} />
+            </Link>
           </div>
           <Input />
           {isUser ? <LoggedInButton /> : <LoginRequiredButton />}
