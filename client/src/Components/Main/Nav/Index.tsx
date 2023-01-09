42cbd4a3dc25b1d873f9571707e117f78ea097e2
sktjgudals <sktjgudals@naver.com>
2023-01-09 16:18:18 +0900

    [FE Feat]  Main nav create

---
+++ client/src/Components/Main/Nav/Index.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import styles from './Styles/index.module.css';
+
+const Index: FC = () => {
+  return <div className={styles.Nav_Container}>ad</div>;
+};
+
+export default Index;
