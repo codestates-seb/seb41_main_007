bb136657a0e972e83aa8f2eed140f275fbc6631a
sktjgudals <sktjgudals@naver.com>
2023-01-09 22:56:57 +0900

    [FE Style]

--- client/src/Components/Main/Nav/NavList.tsx
+++ client/src/Components/Main/Nav/NavList.tsx
@@ -12,14 +12,14 @@ const NavList: FC = () => {
       >
         하우스 시설 자재
       </button>
-      <div className={styles.Nav_Content}>과수 원예자재</div>
-      <div className={styles.Nav_Content}>부직포 제초매트</div>
-      <div className={styles.Nav_Content}>관수 배관자재 </div>
-      <div className={styles.Nav_Content}>차광망 기타망</div>
-      <div className={styles.Nav_Content}>농기계 농업용 자재 </div>
-      <div className={styles.Nav_Content}>방충망 </div>
-      <div className={styles.Nav_Content}>정원 원예용품 </div>
-      <div className={styles.Nav_Content}>생활용품 잡화</div>
+      <button className={styles.Nav_Content}>과수 원예자재</button>
+      <button className={styles.Nav_Content}>부직포 제초매트</button>
+      <button className={styles.Nav_Content}>관수 배관자재 </button>
+      <button className={styles.Nav_Content}>차광망 기타망</button>
+      <button className={styles.Nav_Content}>농기계 농업용 자재 </button>
+      <button className={styles.Nav_Content}>방충망 </button>
+      <button className={styles.Nav_Content}>정원 원예용품 </button>
+      <button className={styles.Nav_Content}>생활용품 잡화</button>
     </div>
   );
 };
