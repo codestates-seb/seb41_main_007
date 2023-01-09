6ef0f2eebbebce30fa51dd4bfc89323478d8e4a6
nakjun12 <111031253+nakjun12@users.noreply.github.com>
2023-01-09 15:09:30 +0900

    [FE Feat] store 문제
    수정중

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './Redux/app/store.tsx';
+import store from './Redux/app/store.tsx';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(
