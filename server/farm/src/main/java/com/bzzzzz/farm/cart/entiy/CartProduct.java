1caf4aecb8d350f1ea89420619d32e5b5f63ca28
Ksiyeong <yuiop2767@naver.com>
2023-01-11 15:29:25 +0900

    [BE Feat] Cart, CartProduct 엔티티 생성
    Member - Cart 연관관계 매핑, setCart 메서드 추가
    MemberService - Cart 생성 로직 추가

---
+++ server/farm/src/main/java/com/bzzzzz/farm/cart/entiy/CartProduct.java
@@ -0,0 +1,29 @@
+package com.bzzzzz.farm.cart.entiy;
+
+import com.bzzzzz.farm.audit.Auditable;
+import com.bzzzzz.farm.product.entity.ProductOption;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class CartProduct extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartProductId;
+
+    @ManyToOne
+    @JoinColumn(name = "CART_ID")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_OPTION_ID")
+    private ProductOption productOption;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
