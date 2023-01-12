f3b603cbbe20b634dedb3195fc2584fcc9c2934c
FordangIT <fordang0819@gmail.com>
2023-01-12 13:11:40 +0900

    [FE Feat] 메인페이지 전체 틀 구상

--- client/src/Pages/Main.tsx
+++ client/src/Pages/Main.tsx
@@ -1,10 +1,16 @@
+import ExampleBest from 'Components/BestProduct/BestCss';
 import { Carousell } from 'Components/Carousel';
+import NewCarousel from 'Components/NewProduct/NewCss';
+import Example from 'Components/Review/ReviewCss';
 import React from 'react';
 
 const Test: React.FC = () => {
   return (
     <>
       <Carousell />
+      <ExampleBest />
+      <NewCarousel />
+      <Example />
     </>
   );
 };
