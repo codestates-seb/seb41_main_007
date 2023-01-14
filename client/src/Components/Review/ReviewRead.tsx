b099e08f303a48f8a7eaae3052ddea5c0d94d051
FordangIT <fordang0819@gmail.com>
2023-01-14 09:26:36 +0900

    [FE Feat]리뷰 사진 속성 수정

--- client/src/Components/Review/ReviewRead.tsx
+++ client/src/Components/Review/ReviewRead.tsx
@@ -1,25 +1,29 @@
 import styled from 'styled-components';
+// import { useCustomQuery } from 'CustomHook/useCustomQuery';
+// import { useCustomMutation } from 'CustomHook/useCustomMutaiton';
+// import { FC, useState } from 'react';
 // import { useFetch } from 'CustomHook/useFetch';
 //import useInput from 'CustomHook/useInput';
 // import { useParams } from 'react-router-dom';
-
 import { useAppSelector } from 'Redux/app/hook';
 import {
   // saveData,
   selectSave,
   // ObjectSaveState,
 } from 'Redux/reducer/objectSaveSlice';
+
 const Container = styled.div``;
 const List = styled.div``;
 
 export const ReviewRead: React.FC = () => {
-  // const { id } = useParams();
-  //const [data, isPending, error] = useFetch('api/revies/${id}/reviews'); // 이걸 리덕스로 담아서 컨트롤을 해라 리뷰 추가했을 때 사용자 입장에선 추가가됨.
-  //const getreview = data[0]; //배열안으로 접근
-  //const [reviewContent, setReview] = useInput('');
+  // const [reviewContent, setReview] = useInput('');
+  // const { data, isLoading, error } = useCustomQuery('/review', '');
   const save = useAppSelector(selectSave);
   const review = save[0];
   //const dispatch = useAppDispatch();
+
+  // if (isLoading) return <></>;
+  // if (error) return <div>error</div>;
   return (
     <>
       <Container>
@@ -47,16 +51,7 @@ export const ReviewRead: React.FC = () => {
             </div>
           </List>
         </>
-        {/* );
-          })} */}
       </Container>
-      {/* {!isPending ? (
-        <Container>
-          <Component data={data} />
-        </Container>
-      ) : (
-        <></>
-      )} */}
     </>
   );
 };
