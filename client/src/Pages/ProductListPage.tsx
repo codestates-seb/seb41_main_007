cc54cdcc8e79b8b4bf3a6300904b59e0be6644a5
sktjgudals <sktjgudals@naver.com>
2023-01-16 16:42:25 +0900

    [FE MISC] 프로덕트리스트 구현

--- client/src/Pages/ProductListPage.tsx
+++ client/src/Pages/ProductListPage.tsx
@@ -1,15 +1,27 @@
 import Empty from 'Components/Common/Empty';
+import ProductList from 'Components/ProductList/ProductList';
 import { useCustomQuery } from 'CustomHook/useCustomQuery';
+import styles from './Styles/Main.module.css';
 import { FC } from 'react';
 import { useParams } from 'react-router-dom';
 
 const ProductListPage: FC = () => {
   let { categoryId } = useParams();
-  const { isLoading, data, error } = useCustomQuery('/', 'qe');
+  const { isLoading, data, error } = useCustomQuery(
+    `/products?categoryId=${categoryId}`,
+    ' categories',
+  );
   if (isLoading) return <Empty />;
-  if (error) return <div>error</div>;
-  console.log(categoryId, data);
-  return <div></div>;
+  if (error || data.data.length === 0) return <div>error</div>;
+  return (
+    <main className={styles.Main_Container}>
+      <ProductList
+        productList={data.data}
+        pageInfo={data.pageInfo}
+        categoryId={categoryId}
+      />
+    </main>
+  );
 };
 
 export default ProductListPage;
