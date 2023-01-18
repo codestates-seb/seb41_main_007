25c47493d306f814ec4c3a9a31bfd502e650b714
sktjgudals <sktjgudals@naver.com>
2023-01-18 16:01:36 +0900

    [FE Style] 스타일링

--- client/src/Components/Header/TopColRight.tsx
+++ client/src/Components/Header/TopColRight.tsx
@@ -1,16 +1,29 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import styles from './Styles/TopColRight.module.css';
+import { Link } from 'react-router-dom';
 const TopColRight: FC = () => {
+  const logoutHandler = (e: React.MouseEvent<Element, MouseEvent>) => {
+    localStorage.removeItem('access_token');
+    window.location.href = '/';
+  };
   return (
-    <div>
+    <div className={styles.Nav_All_Container}>
       <ul className={styles.Nav_Container1}>
-        <li className={styles.Nav_Menu1}>내정보</li>
+        <Link to="/mypage">
+          <li className={styles.Nav_Menu1}>내정보</li>
+        </Link>
         <li className={styles.Nav_Menu1}> |</li>
-        <li className={styles.Nav_Menu1}>로그아웃</li>
+        <button onClick={logoutHandler}>
+          <li className={styles.Nav_Menu1}>로그아웃</li>
+        </button>
       </ul>
       <ul className={styles.Nav_Container2}>
-        <li className={styles.Nav_Menu2}>장바구니</li>
-        <li className={styles.Nav_Menu2}>상품보기</li>
+        <Link to="/products/1">
+          <li className={styles.Nav_Menu2}>상품보기</li>
+        </Link>
+        <Link to="/basket">
+          <li className={styles.Nav_Menu2}>장바구니</li>
+        </Link>
       </ul>
     </div>
   );
