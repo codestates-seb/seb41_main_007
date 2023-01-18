3b0e9ed46a5e8c6f3f0490213223539af99eed62
sktjgudals <sktjgudals@naver.com>
2023-01-18 15:17:50 +0900

    [FE MISC] 리뷰 테스트

--- client/src/Pages/ReactQueryTest.tsx
+++ client/src/Pages/ReactQueryTest.tsx
@@ -1,20 +1,44 @@
+import Empty from 'Components/Common/Empty';
 import { useCustomMutation } from 'CustomHook/useCustomMutaiton';
 import { useCustomQuery } from 'CustomHook/useCustomQuery';
 import { FC, useState } from 'react';
+import NotFoundPage from './NotFoundPage';
+
+interface Review {
+  productId: number;
+  reviewId: number;
+  memberId: number;
+  reviewTitle: string;
+  reviewContent: string;
+  rating: number;
+}
 
 const ReactQueryTest: FC = () => {
   const [text, setText] = useState<string>('');
-  const { data, isLoading, error } = useCustomQuery('/products', 'products');
+  const productId = 1;
+  //해당하는 id 를 ${productId} 넣어서 사용하기
+  const page = 1;
+  const size = 10;
+  const { data, isLoading, error } = useCustomQuery(
+    `/reviews?productId=${productId}&page=${page}&size=${size}`,
+    `reviews?productId=${productId}&page=${page}&size=${size}`,
+  );
 
   const { mutate } = useCustomMutation('/reviews', 'reviews', 'POST');
-
-  if (isLoading) return <></>;
-  if (error) return <div> error</div>;
+  // 아직 api post reviews 미완성
+  if (isLoading) return <Empty />;
+  if (error) return <NotFoundPage />;
+  console.log(data);
   return (
     <div>
       {data &&
-        data.data.map((el: { productId: number; name: string }) => {
-          return <div key={el.productId}> {el.name}</div>;
+        data.data.map((el: Review) => {
+          return (
+            <div key={el.reviewId}>
+              <div>{el.reviewTitle}</div>
+              <div>{el.reviewContent}</div>
+            </div>
+          );
         })}
       <input
         type="text"
