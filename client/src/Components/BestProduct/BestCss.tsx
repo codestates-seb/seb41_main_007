6f3abacbbef561195f661b885e8f606211cb9a73
FordangIT <fordang0819@gmail.com>
2023-01-19 15:33:28 +0900

    [FE Design] 베스트 상품 css

--- client/src/Components/BestProduct/BestCss.tsx
+++ client/src/Components/BestProduct/BestCss.tsx
@@ -93,10 +93,14 @@ const ExampleBest: FC = () => {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-          Best
+        <h2 className="text-4xl font-bold font-serif tracking-tight text-gray-900">
+          베스트 상품
         </h2>
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {/* Component/Common/Product.
+          {data.data && data.data.map((el:any)=>{ */}
+          {/* return <Product key={product.productId} product={product} />          
+          }); } */}
           {products.map((product) => (
             <div key={product.id} className="group relative">
               <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
