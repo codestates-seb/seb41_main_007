f7032c6eb62d2c3448b1b332ce2466cf02653e0f
FordangIT <fordang0819@gmail.com>
2023-01-19 15:37:35 +0900

    [FE Design] 팜피인

--- client/src/Components/NewProduct/StartDive.tsx
+++ client/src/Components/NewProduct/StartDive.tsx
@@ -1,6 +1,6 @@
 const StartDive = () => {
   return (
-    <div className="bg-gray-50 mt-16 mb-4 ">
+    <div className="bg-gray-50 mt-8 mb-4 ">
       <div className="mx-auto max-w-7xl py-12 px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           <span className="block ">Ready to FarmPI in?</span>
