1b1dc52cd526b9278dcd356ae2c054da5b514f6f
bbamsooexit <hiaro66@naver.com>
2023-01-20 14:01:22 +0900

    [BE MISC] OAuthAttributes 코드 수정

--- server/farm/src/main/java/com/bzzzzz/farm/global/security/attribute/OAuthAttributes.java
+++ server/farm/src/main/java/com/bzzzzz/farm/global/security/attribute/OAuthAttributes.java
@@ -34,6 +34,10 @@ public class OAuthAttributes {
 
     public Member toEntity(){
         Member member = new Member();
+        member.setAddress(" ");
+        member.setAge(0);
+        member.setPhone("010-0000-0000");
+        member.setGender(" ");
         member.setName(name);
         member.setEmail(email);
         return member;
