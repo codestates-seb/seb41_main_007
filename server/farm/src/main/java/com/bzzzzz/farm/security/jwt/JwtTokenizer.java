fc290fb3e866bfb6dcb01d4347be1a65d5347d7f
bbamsooexit <hiaro66@naver.com>
2023-01-20 21:53:55 +0900

    [BE MISC] JwtTokenizer 코드 수정

--- server/farm/src/main/java/com/bzzzzz/farm/security/jwt/JwtTokenizer.java
+++ server/farm/src/main/java/com/bzzzzz/farm/security/jwt/JwtTokenizer.java
@@ -43,7 +43,7 @@ public class JwtTokenizer {
                 .collect(Collectors.joining(","));
 
         Map<String, String> claims = new HashMap<>();
-        claims.put("auth", authorities);
+        claims.put("roles", authorities);
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         claims.put("email",(String) oAuth2User.getAttributes().get("email"));
         claims.put("name",(String) oAuth2User.getAttributes().get("name"));
