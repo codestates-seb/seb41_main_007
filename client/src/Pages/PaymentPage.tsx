b18f14fa107b83476030674ac06b8ba44ef25e97
FordangIT <fordang0819@gmail.com>
2023-01-21 03:48:46 +0900

    [FE Feat] 결제 페이지 추가

--- client/src/Pages/PaymentPage.tsx
+++ client/src/Pages/PaymentPage.tsx
@@ -4,9 +4,10 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Totalpay from 'Components/PaymentPage/Totalpay';
 const Container = styled.div`
-  margin: 130px auto;
-  width: 1230px;
+  margin: 200px 0px 200px 300px;
+  width: 830px;
 `;
 const Title = styled.div`
   border-top: 1px solid black;
@@ -17,13 +18,27 @@ const Title = styled.div`
 `;
 
 const PaymentPage: React.FC = () => {
+  const [order, setOrder] = useState<boolean>(true);
   const [address, setAddress] = useState<boolean>(true); //배송지
   const [payment, setPayment] = useState<boolean>(true); //결제수단
   return (
-    <>
+    <div className="flex">
       <Container>
+        <div className="font-semibold py-4 text-2xl mb-3"> 주문서</div>
         <Title>
-          <div className="text-base font-semibold py-4">배송지</div>
+          <div className=" font-semibold py-4 text-xl">
+            주문상품정보 / 총2개
+          </div>
+          <button onClick={() => setOrder(!order)}>
+            {order ? (
+              <FontAwesomeIcon icon={faArrowUp} />
+            ) : (
+              <FontAwesomeIcon icon={faArrowDown} />
+            )}
+          </button>
+        </Title>
+        <Title>
+          <div className=" font-semibold py-4 text-xl">배송지</div>
           <button onClick={() => setAddress(!address)}>
             {address ? (
               <FontAwesomeIcon icon={faArrowUp} />
@@ -34,7 +49,7 @@ const PaymentPage: React.FC = () => {
         </Title>
         {address && <Address />}
         <Title>
-          <div className="text-base font-semibold py-4">결제수단</div>
+          <div className=" font-semibold py-4 text-xl">결제수단</div>
           <button onClick={() => setPayment(!payment)}>
             {payment ? (
               <FontAwesomeIcon icon={faArrowUp} />
@@ -45,7 +60,8 @@ const PaymentPage: React.FC = () => {
         </Title>
         {payment && <Payment />}
       </Container>
-    </>
+      <Totalpay />
+    </div>
   );
 };
 export default PaymentPage;
