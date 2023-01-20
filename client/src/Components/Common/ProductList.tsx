a59fdad89b6a33b4d7afb56cb3f171c1c7bd482a
sktjgudals <sktjgudals@naver.com>
2023-01-20 18:44:36 +0900

    [FE Style] 스타일링 프로덕트

--- client/src/Components/Common/ProductList.tsx
+++ client/src/Components/Common/ProductList.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react';
 import styles from './Styles/ProductList.module.css';
 import Product from './Product';
 import { TYPE_Product } from '../../Types/common/product';
+import useScrollTop from 'CustomHook/useScrollTop';
 
 interface Props {
   products: TYPE_Product[];
 }
 
 const ProductList: FC<Props> = ({ products }) => {
+  useScrollTop();
   return (
     <div className={styles.Products_Container}>
       <div className={styles.Product_List_Container}>
--- client/src/Pages/ProductListPage.tsx
+++ client/src/Pages/ProductListPage.tsx
@@ -1,12 +1,10 @@
 import Empty from 'Components/Common/Empty';
 import ProductList from 'Components/ProductList/ProductList';
 import { useCustomQuery } from 'CustomHook/useCustomQuery';
-import styles from './Styles/ProductList.module.css';
 import { FC } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import CustomTitle from 'Components/Header/CustomTitle';
 import NotFoundPage from './NotFoundPage';
-import { BGcontainer } from 'Components/Common/BGcontainer';
 
 const ProductListPage: FC = () => {
   let { categoryId } = useParams();
@@ -34,22 +32,17 @@ const ProductListPage: FC = () => {
   if (error || data.data.length === 0) return <NotFoundPage />;
 
   return (
-    <main>
-      <BGcontainer>
-        <CustomTitle
-          title={`${categoryId} 상품 리스트 | FarmPi`}
-          description={`카테고리${categoryId}번 상품입니다`}
-        />
-        <div className={styles.Line_Container}>
-          카테고리 {categoryId}번 상품들
-        </div>
-        <ProductList
-          productList={data.data}
-          pageInfo={data.pageInfo}
-          categoryId={categoryId}
-        />
-      </BGcontainer>
-    </main>
+    <>
+      <CustomTitle
+        title={`${categoryId} 상품 리스트 | FarmPi`}
+        description={`카테고리${categoryId}번 상품입니다`}
+      />
+      <ProductList
+        productList={data.data}
+        pageInfo={data.pageInfo}
+        categoryId={categoryId}
+      />
+    </>
   );
 };
 
