3bd409b7e62755b9143c19c7965da2931a777637
Ksiyeong <yuiop2767@naver.com>
2023-01-22 12:49:06 +0900

    [BE Test] getCategories() 테스트
    getCategories() 테스트 케이스 작성
    index.adoc - getCategories() api문서화

--- server/farm/src/test/java/com/bzzzzz/farm/controller/CategoryControllerTest.java
+++ server/farm/src/test/java/com/bzzzzz/farm/controller/CategoryControllerTest.java
@@ -4,6 +4,7 @@ import com.bzzzzz.farm.mapper.CategoryMapper;
 import com.bzzzzz.farm.model.dto.IdRequestDto;
 import com.bzzzzz.farm.model.dto.category.CategoryPatchDto;
 import com.bzzzzz.farm.model.dto.category.CategoryPostDto;
+import com.bzzzzz.farm.model.dto.category.CategoryResponseDto;
 import com.bzzzzz.farm.model.entity.Category;
 import com.bzzzzz.farm.service.CategoryService;
 import com.google.gson.Gson;
@@ -14,10 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
@@ -25,7 +22,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
@@ -34,7 +31,6 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -115,13 +111,74 @@ public class CategoryControllerTest {
     }
 
     @Test
-    @DisplayName("카테고리 목록 가져오기")
+    @DisplayName("카테고리 전체 가져오기")
     void getCategories() throws Exception {
         // given
+        CategoryResponseDto categoryResponseDto1 = CategoryResponseDto
+                .builder()
+                .categoryId(1L)
+                .name("테스트 카테고리1")
+                .sequenceNum(1)
+                .createdAt(LocalDateTime.now())
+                .modifiedAt(LocalDateTime.now())
+                .build();
+        CategoryResponseDto categoryResponseDto2 = CategoryResponseDto
+                .builder()
+                .categoryId(2L)
+                .name("테스트 카테고리2")
+                .sequenceNum(2)
+                .createdAt(LocalDateTime.now())
+                .modifiedAt(LocalDateTime.now())
+                .build();
+        CategoryResponseDto categoryResponseDto3 = CategoryResponseDto
+                .builder()
+                .categoryId(3L)
+                .name("테스트 카테고리3")
+                .sequenceNum(3)
+                .createdAt(LocalDateTime.now())
+                .modifiedAt(LocalDateTime.now())
+                .build();
+
+        List<CategoryResponseDto> response = List.of(categoryResponseDto1, categoryResponseDto2, categoryResponseDto3);
+
+        given(categoryService.findCategories()).willReturn(List.of());
+        given(categoryMapper.categoriesToCategoryResponseDtos(Mockito.anyList())).willReturn(response);
 
         // when
+        ResultActions actions = mockMvc.perform(
+                get("/categories")
+                        .accept(MediaType.APPLICATION_JSON)
+        );
 
         // then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].categoryId").value(categoryResponseDto1.getCategoryId()))
+                .andExpect(jsonPath("$.[0].name").value(categoryResponseDto1.getName()))
+                .andExpect(jsonPath("$.[0].sequenceNum").value(categoryResponseDto1.getSequenceNum()))
+                .andExpect(jsonPath("$.[0].createdAt").isString())
+                .andExpect(jsonPath("$.[0].modifiedAt").isString())
+                .andExpect(jsonPath("$.[1].categoryId").value(categoryResponseDto2.getCategoryId()))
+                .andExpect(jsonPath("$.[1].name").value(categoryResponseDto2.getName()))
+                .andExpect(jsonPath("$.[1].sequenceNum").value(categoryResponseDto2.getSequenceNum()))
+                .andExpect(jsonPath("$.[1].createdAt").isString())
+                .andExpect(jsonPath("$.[1].modifiedAt").isString())
+                .andExpect(jsonPath("$.[2].categoryId").value(categoryResponseDto3.getCategoryId()))
+                .andExpect(jsonPath("$.[2].name").value(categoryResponseDto3.getName()))
+                .andExpect(jsonPath("$.[2].sequenceNum").value(categoryResponseDto3.getSequenceNum()))
+                .andExpect(jsonPath("$.[2].createdAt").isString())
+                .andExpect(jsonPath("$.[2].modifiedAt").isString())
+                .andDo(document(
+                        "getCategories",
+                        preprocessResponse(prettyPrint()),
+                        responseFields(List.of(
+                                fieldWithPath("[*].categoryId").type(JsonFieldType.NUMBER).description("카테고리 식별자"),
+                                fieldWithPath("[*].name").type(JsonFieldType.STRING).description("카테고리 식별자"),
+                                fieldWithPath("[*].sequenceNum").type(JsonFieldType.NUMBER).description("카테고리 정렬을 위한 값(해당 값을 기준으로 정렬됩니다)"),
+                                fieldWithPath("[*].createdAt").type(JsonFieldType.STRING).description("카테고리 생성일자"),
+                                fieldWithPath("[*].modifiedAt").type(JsonFieldType.STRING).description("카테고리 수정일자")
+                        ))
+                ));
 
     }
 
