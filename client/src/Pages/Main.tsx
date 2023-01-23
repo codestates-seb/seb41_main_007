3089ba21fadc843fa3034be6f1169ef62ddfb37b
FordangIT <fordang0819@gmail.com>
2023-01-24 05:33:58 +0900

    [FE Feat]메인페이지 변경

--- client/src/Pages/Main.tsx
+++ client/src/Pages/Main.tsx
@@ -1,26 +1,22 @@
 import { FC } from 'react';
-
-import ExampleBest from 'Components/BestProduct/BestCss';
 import { Carousell } from 'Components/Common/Carousel';
-import NewCarousel from 'Components/NewProduct/NewCss';
 import Example from 'Components/Review/ReviewCss';
 import StartDive from 'Components/NewProduct/StartDive';
 import CustomTitle from 'Components/Header/CustomTitle';
 import Story from 'Components/Main/Story';
-import { BGcontainer } from 'Components/Common/BGcontainer';
+import NewProduct from 'Components/NewProduct/NewProduct';
+import BestProduct from 'Components/BestProduct/BestProduct';
 
 const Main: FC = (): JSX.Element => {
   return (
     <main>
-      <BGcontainer>
-        <CustomTitle title="FarmPi" description={'팜피에 오신걸 환영합니다!'} />
-        <Carousell />
-        <Story />
-        <NewCarousel />
-        <StartDive />
-        <ExampleBest />
-        <Example />
-      </BGcontainer>
+      <CustomTitle title="FarmPi" description={'팜피에 오신걸 환영합니다!'} />
+      <Carousell />
+      <Story />
+      <NewProduct />
+      <StartDive />
+      <BestProduct />
+      <Example />
     </main>
   );
 };
