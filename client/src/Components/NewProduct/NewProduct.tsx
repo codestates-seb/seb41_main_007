f545960c24d4e0e9722b00579ac158633e453b8f
FordangIT <fordang0819@gmail.com>
2023-01-25 16:23:42 +0900

    [FE Feat]신제품

--- client/src/Components/NewProduct/NewProduct.tsx
+++ client/src/Components/NewProduct/NewProduct.tsx
@@ -4,46 +4,13 @@ import 'slick-carousel/slick/slick-theme.css';
 import styled from 'styled-components';
 import { useCustomQuery } from 'CustomHook/useCustomQuery';
 import Data from 'Components/Common/Data';
+import { useNavigate, Link } from 'react-router-dom';
 const Container = styled.div`
   div {
     width: 100%;
     height: 360px;
   }
 `;
-// interface useItemProps {
-//   photo: string;
-//   name: string;
-//   price: number;
-// }
-
-// const Content = styled.div`
-//   img {
-//     width: 280px;
-//     height: 250px;
-//     margin-bottom: 5px;
-//   }
-//   span {
-//     display: block;
-//   }
-// `;
-
-// const Data = ({ photo, name, price }: useItemProps) => {
-//   return (
-//     <>
-//       <Content>
-//         <img src={photo} alt="carousel"></img>
-//         <span className="text-red-600 text-xs mt-4">#신제품</span>
-//         <span className="font-semibold text-sm mt-2">{name}</span>
-//         <span className="text-gray-400 text-xs mt-1">
-//           주문시 10% 추가 할인쿠폰 사용 가능
-//         </span>
-//         <span className="text-red-700 font-semibold text-sm mt-2">
-//           {useNumberComma(price)}원
-//         </span>
-//       </Content>
-//     </>
-//   );
-// };
 const StyledSlider = styled(Slider)`
   position: relative;
   width: 100%;
@@ -91,7 +58,7 @@ const NewProduct: React.FC = (): JSX.Element => {
   );
 
   if (isLoading) return <></>;
-  if (error) return <>error낫음</>;
+  if (error) return <>error났음</>;
   console.log(data.data);
 
   const settings = {
@@ -101,6 +68,7 @@ const NewProduct: React.FC = (): JSX.Element => {
     slidesToScroll: 1,
     arrows: true,
   };
+
   return (
     <>
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
