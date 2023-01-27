5d51a4c74bc3b2a5f97dfaaf39757b07aecdc068
Ksiyeong <yuiop2767@naver.com>
2023-01-27 19:50:23 +0900

    [BE Refactor] @AuthenticationPrincipal 적용
    post(),delete() - pathParameter방식으로 변경

--- server/farm/src/test/java/com/bzzzzz/farm/controller/LikeControllerTest.java
+++ server/farm/src/test/java/com/bzzzzz/farm/controller/LikeControllerTest.java
@@ -1,6 +1,5 @@
 package com.bzzzzz.farm.controller;
 
-import com.bzzzzz.farm.model.dto.like.LikeRequestDto;
 import com.bzzzzz.farm.service.LikeService;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.DisplayName;
@@ -11,13 +10,9 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-
-import java.util.List;
 
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -25,8 +20,8 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -46,18 +41,12 @@ public class LikeControllerTest {
     @DisplayName("제품 추천하기(좋아요)")
     void postLike() throws Exception {
         // given
-        LikeRequestDto request = new LikeRequestDto(1L, 1L);
-
         doNothing().when(likeService).createLike(Mockito.anyLong(), Mockito.anyLong());
-
-        String content = gson.toJson(request);
 
         // when
         ResultActions actions = mockMvc.perform(
-                post("/likes")
-                        .contentType(MediaType.APPLICATION_JSON)
+                post("/likes/{product-id}", 1L)
                         .with(csrf())
-                        .content(content)
         );
 
         // then
@@ -66,12 +55,7 @@ public class LikeControllerTest {
                 .andDo(document(
                         "postLike",
                         preprocessRequest(prettyPrint()),
-                        requestFields(
-                                List.of(
-                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("멤버 식별자 (없어질수도 있어요!)"),
-                                        fieldWithPath("productId").type(JsonFieldType.NUMBER).description("제품 식별자")
-                                )
-                        )
+                        pathParameters(parameterWithName("product-id").description("제품 식별자"))
                 ));
     }
 
@@ -79,18 +63,12 @@ public class LikeControllerTest {
     @DisplayName("추천 취소하기(좋아요취소)")
     void deleteLike() throws Exception {
         // given
-        LikeRequestDto request = new LikeRequestDto(1L, 1L);
-
         doNothing().when(likeService).deleteLike(Mockito.anyLong(), Mockito.anyLong());
-
-        String content = gson.toJson(request);
 
         // when
         ResultActions actions = mockMvc.perform(
-                delete("/likes")
-                        .contentType(MediaType.APPLICATION_JSON)
+                delete("/likes/{product-id}", 1L)
                         .with(csrf())
-                        .content(content)
         );
 
         // then
@@ -99,12 +77,7 @@ public class LikeControllerTest {
                 .andDo(document(
                         "deleteLike",
                         preprocessRequest(prettyPrint()),
-                        requestFields(
-                                List.of(
-                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("멤버 식별자 (없어질수도 있어요!)"),
-                                        fieldWithPath("productId").type(JsonFieldType.NUMBER).description("제품 식별자")
-                                )
-                        )
+                        pathParameters(parameterWithName("product-id").description("제품 식별자"))
                 ));
     }
 }
