848dcc6e9fae3d76297de4ba51461ee9b826d4d7
FordangIT <fordang0819@gmail.com>
2023-01-28 21:11:08 +0900

    [FE Feat]스토리 반응형 완료

--- client/src/Components/Main/Story.tsx
+++ client/src/Components/Main/Story.tsx
@@ -3,42 +3,29 @@ import styled from 'styled-components';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useNavigate } from 'react-router-dom';
+import styles from './Styles/Story.module.css';
 const Container = styled.div`
   margin: 40px auto 0 auto;
   width: 1220px;
   display: flex;
-  .main {
-    width: 100%;
-  }
-  .next {
-    width: 75px;
-    height: 160px;
-    font-size: 32px;
-    color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
 `;
 const Story: FC = () => {
   const navigate = useNavigate();
   return (
-    <>
+    <div className="flex">
       <Container>
-        <div className="border-4 border-green-700 py-6 flex justify-between pr-80 main">
-          <div className="ml-8 ">
-            <div className="bg-green-700 text-sm text-white mb-4 w-14 text-center rounded-xl">
-              이야기
-            </div>
-            <div className="text-green-700 text-2xl font-semibold mb-2">
+        <div className={styles.Story_Container}>
+          <div className={styles.Story_Main_Container}>
+            <div className={styles.Story_Title}>이야기</div>
+            <div className={styles.Story_Content_Title}>
               FarmPi에서 농자재를 구입해야 하는 이유
             </div>
-            <div className="text-green-700">
+            <div className={styles.Story_Content_Content}>
               후회하지 않을 이유를 공개합니다.
             </div>
           </div>
         </div>
-        <div className="bg-green-700 next">
+        <div className={styles.Story_Button}>
           <button
             onClick={() => {
               navigate('/basket');
@@ -48,7 +35,7 @@ const Story: FC = () => {
           </button>
         </div>
       </Container>
-    </>
+    </div>
   );
 };
 export default Story;
