8f567b9413959d4493448a0101c0d23ee3889ba6
bbamsooexit <hiaro66@naver.com>
2023-01-28 23:13:16 +0900

    [BE MISC] JWT 토큰 유효시간 2시간으로 설정

--- server/farm/src/main/java/com/bzzzzz/farm/common/security/jwt/JwtTokenizer.java
+++ server/farm/src/main/java/com/bzzzzz/farm/common/security/jwt/JwtTokenizer.java
@@ -26,14 +26,14 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
+@Getter
 @Component
 @RequiredArgsConstructor
 public class JwtTokenizer {
-    @Getter
     @Value("${jwt.key}")
     private String secretKey;
 
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60;    // 액세스 토큰 만료 시간 : 1시간
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 2;    // 액세스 토큰 만료 시간 : 2시간
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; // 리프레쉬 토큰 만료 시간 : 7일
     private final MemberRepository memberRepository;
     public String encodeBase64SecretKey(String secretKey) {
