61b731ba48ba96a1d62829fc38efb1c8ddeb6050
FordangIT <fordang0819@gmail.com>
2023-01-30 07:46:33 +0900

    [FE Feat]캐러셀 반응형 80% 진행

--- client/src/Components/Common/Carousel.tsx
+++ client/src/Components/Common/Carousel.tsx
@@ -1,7 +1,6 @@
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 import styled from 'styled-components';
-import styles from './Styles/Carousel.module.css';
 
 interface useCarousel {
   timer: number;
@@ -32,18 +31,22 @@ const Item = ({
       <Size>
         <Paper>
           <div className={back}>
-            <div className={styles.Carousel_Entire_Content}>
-              <div className={styles.Carousel_Short}>{short}</div>
-              <h1 className={styles.Carousel_Title}>{title}</h1>
-              <p className={styles.Carousel_Content}>{content}</p>
-              <p className={styles.Carousel_Content2}>{content2}</p>
+            <div className="object-none">
+              <div className="absolute lg:inset-y-28 lg:left-60 md:inset-y-14 md:left-20 sm:inset-y-4 inset-y-4 left-14">
+                {short}
+              </div>
+              <h1 className="absolute lg:inset-y-44 lg:left-60 lg:text-5xl md:inset-y-28 md:text-4xl md:left-20 font-semibold inset-y-16 text-3xl left-14">
+                {title}
+              </h1>
+              <p className="absolute lg:inset-y-64 lg:left-60 lg:text-2xl md:inset-y-44 md: text-xl md:left-20 inset-y-32 left-14">
+                {content}
+              </p>
+              <p className="absolute lg:inset-y-72 lg:left-60 lg:text-2xl md:inset-y-52 md:left-20 md:text-xl inset-y-40 text-xl left-14">
+                {content2}
+              </p>
             </div>
-            <div className={styles.Carousel_Img_Container}>
-              <img
-                src={url}
-                alt="carousel"
-                className={styles.Carousel_Img}
-              ></img>
+            <div className="">
+              <img src={url} alt="carousel" className={place}></img>
             </div>
           </div>
         </Paper>
@@ -53,7 +56,14 @@ const Item = ({
 };
 export const Size = styled.div`
   img {
-    height: 100%;
+    @media screen and (max-width: 760px) {
+      height: 450px;
+      padding-top: 190px;
+    }
+    @media screen and (min-width: 761px) {
+      height: 450px;
+    }
+    height: 480px;
     width: 100%;
   }
   .background1 {
@@ -107,24 +117,22 @@ export const Carousell = (props: any) => {
         'object-none sm:object-left md:object-center lg:object-right lg:pr-72',
     },
     {
-      url: '/image/timer.gif',
+      url: `https://cdn.inflearn.com/public/main_sliders/eead4cae-85b7-4faf-b8e4-6d7680037c2d/%5B%E1%84%86%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%92%E1%85%B5%E1%84%8B%E1%85%A5%E1%84%85%E1%85%A9%5D%E1%84%8C%E1%85%B5%E1%84%80%E1%85%B3%E1%86%B7%E1%84%92%E1%85%A1%E1%86%AF%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%8C%E1%85%AE%E1%86%BC_521.gif`,
       title: '지금 할인 중인 상품💸',
       content: '모든 전 상품의 할인 기회를',
       content2: '놓치지 마세요!',
       short: <Short2 />,
       back: 'background2',
-      place:
-        'object-none sm:object-left md:object-center lg:object-right lg:pr-72',
+      place: 'object-none sm: object-left md:object-center lg:object-right',
     },
     {
-      url: '/image/hand.gif',
+      url: `https://media1.giphy.com/media/QHsMhRci6hfSqerbXy/giphy.gif?cid=ecf05e47wmvdyj0vhzmk2imbnw66zm7wyykoysob6n3jc6ls&rid=giphy.gif&ct=g`,
       title: '농부들을 지지하는 팜피🐣',
       content: '농부들의 마음을 아는 팜피와',
       content2: '농사를 시작해보세요!',
       short: <Short3 />,
       back: 'background3',
-      place:
-        'object-none sm:object-left md:object-right xl:object-right xl:pr-64',
+      place: 'object-none sm:object-left lg:object-right lg:pr-60',
     },
   ];
   return (
