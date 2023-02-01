b295f013dabedba797aa10c372da5a202837c773
Ksiyeong <yuiop2767@naver.com>
2023-02-01 16:37:32 +0900

    [BE Feat] cacheController
    캐시 전체 초기화 메서드 추가

--- server/farm/src/main/java/com/bzzzzz/farm/controller/CacheController.java
+++ server/farm/src/main/java/com/bzzzzz/farm/controller/CacheController.java
@@ -1,20 +1,16 @@
 package com.bzzzzz.farm.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Validated
-@RequiredArgsConstructor
-public class MainController {
+public class CacheController {
     @GetMapping("/caches/initialization")
-    @CacheEvict(cacheNames = "test", allEntries = true)
+    @CacheEvict(value = {"findProducts", "findCategories", "findReviewsOrderByReviewId"}, allEntries = true)
     public ResponseEntity initializeCache() {
-        return new ResponseEntity("캐쉬가 초기화 되었습니다.", HttpStatus.OK);
+        return new ResponseEntity("캐시가 초기화 되었습니다.", HttpStatus.OK);
     }
 }
