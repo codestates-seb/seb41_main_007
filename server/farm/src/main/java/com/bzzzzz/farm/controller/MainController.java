6304b426558d1f00e1b73a10019bb03da3dcc1c3
Ksiyeong <yuiop2767@naver.com>
2023-02-01 15:24:12 +0900

    [BE Refactor] 미사용 메서드 삭제

--- server/farm/src/main/java/com/bzzzzz/farm/controller/MainController.java
+++ server/farm/src/main/java/com/bzzzzz/farm/controller/MainController.java
@@ -1,51 +1,20 @@
 package com.bzzzzz.farm.controller;
 
-import com.bzzzzz.farm.mapper.ReviewMapper;
-import com.bzzzzz.farm.model.dto.product.ProductSimpleResponseDto;
-import com.bzzzzz.farm.model.dto.review.ReviewSimpleResponseDto;
-import com.bzzzzz.farm.service.ProductService;
-import com.bzzzzz.farm.service.ReviewService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @RestController
 @Validated
 @RequiredArgsConstructor
 public class MainController {
-    private final ProductService productService;
-    private final ReviewService reviewService;
-    private final ReviewMapper reviewMapper;
-
-    @GetMapping
-    @Cacheable(value = "getMain")
-    public ResponseEntity getMain() {
-        /**
-         * 좋아요 높은 순 10개
-         * 신상품 7개
-         * 판매량 순 20개 Todo: CacheEvict 적용할 것
-         * 최근 리뷰 4개
-         * */
-        Map<String, Object> response = new HashMap<>();
-
-        List<ProductSimpleResponseDto> orderByLikeCount = productService.findProducts(0, 10, "likeCount", "descending", null, null).getContent();
-        List<ProductSimpleResponseDto> orderByProductId = productService.findProducts(0, 7, "productId", "descending", null, null).getContent();
-        List<ProductSimpleResponseDto> orderBySoldCount = productService.findProducts(0, 20, "soldCount", "descending", null, null).getContent();
-        List<ReviewSimpleResponseDto> reviews = reviewMapper.reviewsToReviewSimpleResponseDtos(reviewService.findReviewsOrderByReviewId());
-
-        response.put("orderByLikeCount", orderByLikeCount);
-        response.put("orderByProductId", orderByProductId);
-        response.put("orderBySoldCount", orderBySoldCount);
-        response.put("reviews", reviews);
-        return new ResponseEntity(response, HttpStatus.OK);
+    @GetMapping("/caches/initialization")
+    @CacheEvict(cacheNames = "test", allEntries = true)
+    public ResponseEntity initializeCache() {
+        return new ResponseEntity("캐쉬가 초기화 되었습니다.", HttpStatus.OK);
     }
-
 }
